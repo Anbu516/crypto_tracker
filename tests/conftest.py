e51ad533d7887999eacd8447e4946af4512bd185
Anbarasu <anbarasud45@gmail.com>
2026-04-03 16:14:48 +0530

    pytest imports change

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.pool import StaticPool
-from ..app import main,database,config
-from main import app
-from database import Base, get_db
-from config import settings
+from app import main,database,config
+from app.main import app
+from app.database import Base, get_db
+from app.config import settings
 from unittest.mock import AsyncMock, patch
 from contextlib import ExitStack
 
